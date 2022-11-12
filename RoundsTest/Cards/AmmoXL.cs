bac62b56d8156ca5a1d73a1812c6af3608c057e6
K1SELKA@yandex.ru <K1SELKA@yandex.ru>
2022-11-13 02:33:19 +0300

    firstStep

---
+++ RoundsTest/Cards/AmmoXL.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnboundLib;
+using UnboundLib.Cards;
+using UnityEngine;
+
+namespace RoundsTest.Cards
+{
+    public class AmmoXL : CustomCard
+    {
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers, Block block)
+        {
+            //Edits values on card itself, which are then applied to the player in `ApplyCardStats`
+        }
+
+        public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            gunAmmo.maxAmmo += 10;
+        }
+
+        public override void OnRemoveCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            gunAmmo.maxAmmo -= 10;
+        }
+
+        protected override string GetTitle()
+        {
+            return "AmmoXL";
+        }
+
+        protected override string GetDescription()
+        {
+            return "Buff maxAmmo";
+        }
+
+        protected override GameObject GetCardArt()
+        {
+            return null;
+        }
+
+        protected override CardInfo.Rarity GetRarity()
+        {
+            return CardInfo.Rarity.Common;
+        }
+
+        protected override CardInfoStat[] GetStats()
+        {
+            return new CardInfoStat[]
+            {
+                new CardInfoStat()
+                {
+                    positive = true,
+                    stat = "Ammo",
+                    amount = "+10",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                }
+            };
+        }
+
+        protected override CardThemeColor.CardThemeColorType GetTheme()
+        {
+            return CardThemeColor.CardThemeColorType.ColdBlue;
+        }
+
+        public override string GetModName()
+        {
+            return "RT";
+        }
+    }
+}
+
+
--- RoundsTest/Class1.cs
+++ RoundsTest/Class1.cs
@@ -1,9 +1,45 @@
-﻿using System;
+﻿using BepInEx;
+using UnboundLib;
+using UnboundLib.Cards;
+using RoundsTest.Cards;
+using HarmonyLib;
+using CardChoiceSpawnUniqueCardPatch.CustomCategories;
 
 namespace RoundsTest
 {
-    public class Class1
-    {
+    // These are the mods required for our mod to work
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.moddingutils", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch", BepInDependency.DependencyFlags.HardDependency)]
+    // Declares our mod to Bepin
+    [BepInPlugin(ModId, ModName, Version)]
+    // The game our mod is associated with
+    [BepInProcess("Rounds.exe")]
 
+    public class RoundsTest : BaseUnityPlugin
+    {
+        #region Singletone
+
+        public static RoundsTest Instance { get; private set; }
+
+        #endregion
+
+        private const string ModId = "com.aderom.rounds.RoundsTest";
+        private const string ModName = "RoundsTest";
+        public const string Version = "1.0.0"; // What version are we on (major.minor.patch)?
+        public const string ModInitials = "RT";
+
+        void Awake()
+        {
+            // Use this to call any harmony patch files your mod may have
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+
+        void Start()
+        {
+            Instance = this;
+            CustomCard.BuildCard<AmmoXL>();
+        }
     }
 }
