f14ede9bed244fe2e7dc917dd0ba3f5dc0be6132
K1SELKA@yandex.ru <K1SELKA@yandex.ru>
2022-11-17 15:36:51 +0300

    add SawnOff

---
+++ RoundsTest/Cards/Weapon_common/SawnOff.cs
@@ -0,0 +1,128 @@
+﻿using BrutalGun;
+using CardChoiceSpawnUniqueCardPatch.CustomCategories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnboundLib;
+using UnboundLib.Cards;
+using UnityEngine;
+
+namespace BrutalGun.Cards
+{
+    public class SawnOff : CustomCard
+    {
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers, Block block)
+        {
+            cardInfo.categories = new CardCategory[] { MyCategories.Weapon };
+        }
+
+        public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            //show stats
+            gun.damage = 1f;
+            gun.attackSpeed = 0.01f;
+            gunAmmo.maxAmmo = 6;
+            gunAmmo.reloadTimeAdd = -0.3f;
+
+            // hide stats
+            gun.dontAllowAutoFire = true;
+            gun.projectileSpeed = 1.5f;
+            gun.gravity = 0.5f;
+            gun.spread = 0.2f;
+
+            //shotgun
+            gun.bursts = 1;
+            gun.numberOfProjectiles = 3;
+        }
+
+        public override void OnRemoveCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            //show stats
+            gun.damage = 1f;
+            gun.attackSpeed = 1f;
+            gunAmmo.maxAmmo = 3;
+            gunAmmo.reloadTimeAdd = +0.3f;
+
+            // hide stats
+            gun.dontAllowAutoFire = true;
+            gun.projectileSpeed = 1f;
+            gun.gravity = 1f;
+            gun.spread = 0f;
+
+            //shotgun
+            gun.bursts = 0;
+            gun.numberOfProjectiles = 1;
+        }
+
+        protected override string GetTitle()
+        {
+            return "Sawn-Off";
+        }
+
+        protected override string GetDescription()
+        {
+            return "CartDescription";
+        }
+
+        protected override GameObject GetCardArt()
+        {
+            return null;
+        }
+
+        protected override CardInfo.Rarity GetRarity()
+        {
+            return CardInfo.Rarity.Common;
+        }
+
+        protected override CardInfoStat[] GetStats()
+        {
+            return new CardInfoStat[]
+            {
+                new CardInfoStat()
+                {
+                    positive = true,
+                    stat = "DMG",
+                    amount = "55x3",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                },
+
+                new CardInfoStat()
+                {
+                    positive = true,
+                    stat = "ATKSPD",
+                    amount = "100",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                },
+
+                new CardInfoStat()
+                {
+                    positive = true,
+                    stat = "AMMO",
+                    amount = "2",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                },
+
+                new CardInfoStat()
+                {
+                    positive = true,
+                    stat = "Reload Time",
+                    amount = "0.7s",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                },
+            };
+        }
+
+        protected override CardThemeColor.CardThemeColorType GetTheme()
+        {
+            return CardThemeColor.CardThemeColorType.DestructiveRed;
+        }
+
+        public override string GetModName()
+        {
+            return BrutalGun.BrutalGunMain.MOD_INITIALS;
+        }
+    }
+}
+
