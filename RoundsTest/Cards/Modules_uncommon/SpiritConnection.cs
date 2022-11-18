8dd8a14ad97ce1ba22dffa672f9f62ef1fd35d6c
K1SELKA@yandex.ru <K1SELKA@yandex.ru>
2022-11-18 13:09:36 +0300

    add

---
+++ RoundsTest/Cards/Modules_uncommon/SpiritConnection.cs
@@ -0,0 +1,87 @@
+﻿using CardChoiceSpawnUniqueCardPatch.CustomCategories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnboundLib;
+using UnboundLib.Cards;
+using UnityEngine;
+using BrutalGun;
+
+namespace BrutalGun.Cards
+{
+    public class SpiritConnection : CustomCard
+    {
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers, Block block)
+        {
+            cardInfo.categories = new CardCategory[] { MyCategories.Module };
+
+            statModifiers.lifeSteal = 0.2f;
+            gun.multiplySpread = 1.05f;
+        }
+
+        public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            
+        }
+
+        public override void OnRemoveCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            //Run when the card is removed from the player
+        }
+
+        protected override string GetTitle()
+        {
+            return "Spirit Connection";
+        }
+
+        protected override string GetDescription()
+        {
+            return "CardDescription";
+        }
+
+        protected override GameObject GetCardArt()
+        {
+            return null;
+        }
+
+        protected override CardInfo.Rarity GetRarity()
+        {
+            return CardInfo.Rarity.Uncommon;
+        }
+
+        protected override CardInfoStat[] GetStats()
+        {
+            return new CardInfoStat[]
+            {
+                new CardInfoStat()
+                {
+                    positive = true,
+                    stat = "LifeSteal",
+                    amount = "+20%",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                },
+
+                new CardInfoStat()
+                {
+                    positive = true,
+                    stat = "Accuracy",
+                    amount = "-5%",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                }
+            };
+        }
+
+        protected override CardThemeColor.CardThemeColorType GetTheme()
+        {
+            return CardThemeColor.CardThemeColorType.ColdBlue;
+        }
+
+        public override string GetModName()
+        {
+            return BrutalGun.BrutalGunMain.MOD_INITIALS;
+        }
+    }
+}
+
