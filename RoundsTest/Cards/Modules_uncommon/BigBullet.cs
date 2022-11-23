22fd7686bf781e501c34260d9dad6abdfeff369e
K1SELKA@yandex.ru <K1SELKA@yandex.ru>
2022-11-23 16:55:19 +0300

    create card

---
+++ RoundsTest/Cards/Modules_uncommon/BigBullet.cs
@@ -0,0 +1,105 @@
+﻿using CardChoiceSpawnUniqueCardPatch.CustomCategories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnboundLib;
+using UnboundLib.Cards;
+using UnityEngine;
+using BrutalGun;
+
+namespace BrutalGun.Cards
+{
+    public class BigBullet : CustomCard
+    {
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers, Block block)
+        {
+            cardInfo.categories = new CardCategory[] { MyCategories.Module };
+
+            gun.bulletDamageMultiplier = 1.15f;
+            gun.projectileSpeed = 0.85f;
+            gun.gravity = 1.2f;
+            gun.projectileSize = 1.25f;
+        }
+
+        public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            
+        }
+
+        public override void OnRemoveCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            //Run when the card is removed from the player
+        }
+
+        protected override string GetTitle()
+        {
+            return "Big Bullet";
+        }
+
+        protected override string GetDescription()
+        {
+            return "Fat and angry bullets";
+        }
+
+        protected override GameObject GetCardArt()
+        {
+            return null;
+        }
+
+        protected override CardInfo.Rarity GetRarity()
+        {
+            return CardInfo.Rarity.Uncommon;
+        }
+
+        protected override CardInfoStat[] GetStats()
+        {
+            return new CardInfoStat[]
+            {
+                new CardInfoStat()
+                {
+                    positive = true,
+                    stat = "DMG",
+                    amount = "15%",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                },
+
+                new CardInfoStat()
+                {
+                    positive = true,
+                    stat = "Bullet Size",
+                    amount = "+25%",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                },
+
+                new CardInfoStat()
+                {
+                    positive = false,
+                    stat = "Bullet speed",
+                    amount = "-15%",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                },
+
+                new CardInfoStat()
+                {
+                    positive = false,
+                    stat = "Gravity",
+                    amount = "+20%",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                }
+            };
+        }
+
+        protected override CardThemeColor.CardThemeColorType GetTheme()
+        {
+            return CardThemeColor.CardThemeColorType.ColdBlue;
+        }
+
+        public override string GetModName()
+        {
+            return BrutalGun.BrutalGunMain.MOD_INITIALS;
+        }
+    }
+}
+
