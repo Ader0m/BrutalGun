cef3394920735fa421f08932fc2ed3bb967e2069
Ader0m <k1selka@yandex.ru>
2022-12-09 13:30:30 +0300

    GetExplosion move

---
+++ RoundsTest/BrutalTools.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace BrutalGun
+{
+    public static class BrutalTools
+    {
+        public static (GameObject AddToProjectile, GameObject effect, Explosion explosion) LoadExplosionElements()
+        {
+            GameObject explosiveBullet = (GameObject)Resources.Load("0 cards/Explosive bullet");
+            Gun explosiveGun = explosiveBullet.GetComponent<Gun>();
+
+            GameObject A_ExplosionSpark = explosiveGun.objectsToSpawn[0].AddToProjectile;
+            GameObject explosionCustom = MonoBehaviour.Instantiate(explosiveGun.objectsToSpawn[0].effect);
+            explosionCustom.transform.position = new UnityEngine.Vector3(1000, 0, 0);
+            explosionCustom.hideFlags = HideFlags.HideAndDontSave;
+            explosionCustom.name = "customExpl";
+
+            UnityEngine.Object.DestroyImmediate(explosionCustom.GetComponent<RemoveAfterSeconds>());
+
+
+            return (A_ExplosionSpark, explosionCustom, explosionCustom.GetComponent<Explosion>());
+        }
+    }
+}
