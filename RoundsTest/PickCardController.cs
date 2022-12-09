e154efae2c1e8c0ec3c2ef6f578234f327236a53
Ader0m <k1selka@yandex.ru>
2022-12-09 13:30:41 +0300

    add PickCardController

---
+++ RoundsTest/PickCardController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BrutalGun.Cards;
+
+namespace BrutalGun
+{
+    public static class PickCardController
+    {
+        public static void InitPlayer(Player player)
+        {
+            ModdingUtils.Extensions.CharacterStatModifiersExtension.GetAdditionalData(player.data.stats).blacklistedCategories.Add(MyCategories.Vampire);
+        }
+
+        public static void SetVampirePick(Player player)
+        {
+            ModdingUtils.Extensions.CharacterStatModifiersExtension.GetAdditionalData(player.data.stats).blacklistedCategories.Add(MyCategories.Weapon); 
+            ModdingUtils.Extensions.CharacterStatModifiersExtension.GetAdditionalData(player.data.stats).blacklistedCategories.Add(MyCategories.Human); 
+            ModdingUtils.Extensions.CharacterStatModifiersExtension.GetAdditionalData(player.data.stats).blacklistedCategories.Remove(MyCategories.Vampire);
+        }
+    }
+}
