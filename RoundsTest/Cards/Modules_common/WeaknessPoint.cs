3fa61ad3a42da9faac6c40d7393ff29e6617beec
K1SELKA@yandex.ru <K1SELKA@yandex.ru>
2023-01-08 20:45:14 +0300

    fix

--- RoundsTest/Cards/Modules_common/WeaknessPoint.cs
+++ RoundsTest/Cards/Modules_common/WeaknessPoint.cs
@@ -27,10 +27,10 @@ namespace BrutalGun.Cards
 
         public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers, Block block)
         {
-            cardInfo.categories = new CardCategory[] { MyCategories.Module };
+            cardInfo.categories = new CardCategory[] { MyCategories.Module, MyCategories.Human };
             cardInfo.allowMultiple = true;
 
-            gun.bulletDamageMultiplier = 0.05f;
+            gun.bulletDamageMultiplier = 1.05f;
         }
     }
 }
